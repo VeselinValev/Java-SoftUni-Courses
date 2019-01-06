b234185f57e20ef9f88151318d2e3d8dfa9e0f59
Veselin Valev <37142700+VeselinValev@users.noreply.github.com>
2019-01-06 19:56:46 +0200

    New module

--- Data-Structures- Java Exercises/6. Red-Black trees/Exercises/main/Main.java
+++ Data-Structures- Java Exercises/6. Red-Black trees/Exercises/main/Main.java
@@ -5,45 +5,7 @@ import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = reader.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        ArrayDeque<Integer> numbers = new ArrayDeque<>();
-        if (n == m) {
-            System.out.println(n);
-        } else if  (n < m){
-            numbers.addFirst(m);
-            while (m != n) {
-                if (m < 6){
-                    while(m != n){
-                        if (m - 2 >= n){
-                            m = m- 2;
-                        }else if (m - 1 >= n){
-                            m = m - 1;
-                        }
-                        numbers.addLast(m);
-                    }
-                    break;
-                }
-                if (m / 2 >= n) {
-                    if (m % 2 != 0) {
-                        m = m - 1;
-                        numbers.addLast(m);
-                    }
-                    m = m / 2;
-                } else if (m - 2 >= n) {
-                    m = m - 2;
-                } else if (m - 1 >= n) {
-                    m = m - 1;
-                }
-                numbers.addLast(m);
-            }
-            StringBuilder sb = new StringBuilder();
-            while (!numbers.isEmpty()) {
-                sb.append(numbers.pollLast()).append(" -> ");
-            }
-            System.out.println(sb.substring(0, sb.length() - 4));
-        }
+
+
     }
 }
